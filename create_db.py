11db68aab7cf01b2303f16ee5e5bdcb3b5c972a9
Dmitry_Anoshin <dimitryanoshin@yandex.ru>
2023-05-24 01:29:02 +0300

    do not touch xd

--- create_db.py
+++ create_db.py
@@ -1,5 +1,8 @@
 from database import Base, engine
 
+# Do not fucking touch, shit does not work without it :)
+from models import User
+
 
 print('Create database...')
 Base.metadata.create_all(engine)
